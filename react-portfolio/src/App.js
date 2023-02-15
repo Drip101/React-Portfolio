fc34a9dba2bb72f13b9188eac4a504e2eeabf2c1
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2023-02-15 12:49:20 -0500

    update my portfolio

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header'
 import Bio from './components/Bio';
 import AboutMe from './components/AboutMe';
+import MyPortfolio from './components/Myportfolio';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Bio/>
       <AboutMe/>
+      <MyPortfolio/>
     </div>
   );
 }
